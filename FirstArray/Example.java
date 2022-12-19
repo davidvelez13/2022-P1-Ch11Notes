c4c4e6782ffbda2dd9e179c683aa8fd0328254ab
21377 <t@vtsd.com>
2022-12-19 07:57:31 -0500

    bruh

---
+++ FirstArray/Example.java
@@ -0,0 +1,17 @@
+package FirstArray;
+
+public class Example {
+    public static void main(String args[]){
+
+        // Arrays store one datatype
+        int numberofchristmasgifts[];
+        numberofchristmasgifts = new int[2];
+
+        numberofchristmasgifts[0] = 7;
+        numberofchristmasgifts[1] = 3;
+
+        System.out.println(numberofchristmasgifts[0]);
+
+
+    }
+}
