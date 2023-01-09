cf1da1d24db728ececa1ebf59deaa194c8968940
21377 <t@vtsd.com>
2023-01-09 07:58:27 -0500

    bruh

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,33 @@
+package Searching;
+
+import java.util.Random;
+
+public class LinearSearch {
+    static int numList[] = new int[1000];
+    public static void man(String args[]) {
+        Random rand = new Random();
+        for (int k = 0; k < numList.length; k++) {
+            numList[k] = rand.nextInt(1000);
+        }
+        for (int k = 0; k < numList.length; k++) {
+            System.out.print(numList[k] + " ");
+        }
+        linearSearch(974);
+    }
+    public static int linearSearch(int lookup){
+        for (int k = 0; k < numList.length; k++) {
+            if(numList[k] == lookup){
+                return k;
+        }
+
+        }
+        }
+        }
+
+
+
+
+
+
+
+
